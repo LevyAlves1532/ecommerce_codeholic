1b7260c7fe83f7e6922a99f26cd9b5ab14d6184b
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-01-23 08:44:07 -0300

    Criando aba para variações de produto

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -7,6 +7,7 @@ use App\Enums\RolesEnum;
 use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\Pages\EditProduct;
 use App\Filament\Resources\ProductResource\Pages\ProductImages;
+use App\Filament\Resources\ProductResource\Pages\ProductVariationTypes;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Filament\Facades\Filament;
@@ -158,6 +159,7 @@ class ProductResource extends Resource
             'create' => Pages\CreateProduct::route('/create'),
             'edit' => Pages\EditProduct::route('/{record}/edit'),
             'images' => Pages\ProductImages::route('/{record}/images'),
+            'variation-types' => Pages\ProductVariationTypes::route('/{record}/variation-types'),
         ];
     }
 
@@ -166,6 +168,7 @@ class ProductResource extends Resource
         return $page->generateNavigationItems([
             EditProduct::class, 
             ProductImages::class,
+            ProductVariationTypes::class,
         ]);
     }
 
